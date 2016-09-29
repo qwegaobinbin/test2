3878f860768594ef28d3245d69da18a91a52694a
qwegaobinbin2 <471822745@qq.com>
2016-09-29 10:45:51 +0800

    trgtrdg

---
+++ ConsoleApplication1/ConsoleApplication1/ClassA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class ClassA : AInterface
+    {
+       
+        public void fang1()
+        {
+            string a = "你好A";
+        }
+    }
+}
