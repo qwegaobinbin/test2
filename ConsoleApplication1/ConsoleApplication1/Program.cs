9c917829645456b31629ca16c84a70fbde96a46d
gaobinbin <471822745@qq.com>
2016-09-30 09:08:41 +0800

    dgg

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApplication1
             AInterface aInterface = new ClassC();
             aInterface.fang1();
 
-            string str = "123";
+            string str = "12ooppppp3";
             str = "123";
             str = "123";
 
