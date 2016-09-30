dd17b4465bb57df0f77018b86b421dbddc34316d
gaobinbin <471822745@qq.com>
2016-09-30 09:03:15 +0800

    vvvv

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -44,7 +44,7 @@ namespace ConsoleApplication1
 
 
             AopClass aopClass = new AopClass();
-            aopClass.Say("ddpppp");
+            aopClass.Say("ddpffffffffffffppp");
             aopClass.Hello();
 
         }
