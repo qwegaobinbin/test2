3fe99f6f8e4c9ec3a387e97a331e6225ff7ec7aa
gaobinbin2 <471822745@qq.com>
2016-10-09 13:40:23 +0800

    aop修改

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/AopHelper/AopPerson.cs
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/AopHelper/AopPerson.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1.AopHelper
 {
-    public class AopPerson : AopProxy1Provider<AopPerson> 
+    public class AopPerson : AopProxy1Provider<Person> 
     {
         public AopPerson(Type serverType)
             : base(serverType) { }
@@ -14,7 +15,9 @@ namespace ConsoleApplication1.AopHelper
 
         public void BeforeHello(string ni, out string ddd)
         {
-            ddd = string.Empty;
+            ddd = "rr";
+            Console.Write("ddddddddddddddddddBeforeHello");
+            Thread.Sleep(200);
         }
 
         //输出设置
